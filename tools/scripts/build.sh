6d277f2ed1b3a35fc9f255f8c15bfd0bfad29866
wbh1328551759 <1328551759@qq.com>
2024-01-19 17:21:06 +0800

    feat: fix build

--- tools/scripts/build.sh
+++ tools/scripts/build.sh
@@ -11,10 +11,11 @@ dotenv \
 yarn svg:build-sprite
 echo ""
 
+echo 'tags: ' $(git describe --tags --abbrev=0)
 # generate envs.js file and run the app
 dotenv \
   -v NEXT_PUBLIC_GIT_COMMIT_SHA=$(git rev-parse --short HEAD) \
-  -v NEXT_PUBLIC_GIT_TAG=$(git describe --tags --abbrev=0) \
+  -v NEXT_PUBLIC_GIT_TAG=$(git describe --tags --always) \
   -e .env.secrets \
   -e .env.development.local \
   -e .env.local \
