1abc96ac978da468aa193f27a1102085c93c10e6
wbh1328551759 <1328551759@qq.com>
2024-01-19 16:57:31 +0800

    feat: update config

--- deploy/scripts/download_assets.sh
+++ deploy/scripts/download_assets.sh
@@ -36,7 +36,7 @@ get_target_filename() {
     local name_prefix="${env_var#NEXT_PUBLIC_}"
     local name_suffix="${name_prefix%_URL}"
     local name_lc="$(echo "$name_suffix" | tr '[:upper:]' '[:lower:]')"
-    
+
     # Check if the URL starts with "file://"
     if [[ "$url" == file://* ]]; then
         # Extract the local file path
@@ -54,7 +54,7 @@ get_target_filename() {
 
     # Convert the extension to lowercase
     extension=$(echo "$extension" | tr '[:upper:]' '[:lower:]')
-    
+
     # Construct the custom file name
     echo "$name_lc.$extension"
 }
