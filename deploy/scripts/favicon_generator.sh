2d6dac7a99475f98decac667a779e3162e225782
wbh1328551759 <1328551759@qq.com>
2024-01-22 18:15:29 +0800

    feat: fix fetch

--- deploy/scripts/favicon_generator.sh
+++ deploy/scripts/favicon_generator.sh
@@ -4,19 +4,22 @@ master_url="${FAVICON_MASTER_URL:-$NEXT_PUBLIC_NETWORK_ICON}"
 #master_url=""
 export MASTER_URL="$master_url"
 
-cd ./deploy/tools/favicon-generator
-./script.sh
-if [ $? -ne 0 ]; then
-    cd ../../../
-    exit 1
-else
-    cd ../../../
-    favicon_folder="./public/favicon/"
+dotenv \
+  -v MASTER_URL=$master_url \
+  -e $secrets_file \
+  -- bash -c 'cd ./deploy/tools/favicon-generator && ./script.sh'
+cd -
+#if [ $? -ne 0 ]; then
+#    cd ../../../
+#    exit 1
+#else
+#    cd ../../../
+favicon_folder="./public/favicon/"
 
-    echo "⏳ Replacing default favicons with freshly generated pack..."
-    if [ -d "$favicon_folder" ]; then
-        rm -r "$favicon_folder"
-    fi
-    mkdir -p "$favicon_folder"
-    cp -r ./deploy/tools/favicon-generator/output/* "$favicon_folder"
+echo "⏳ Replacing default favicons with freshly generated pack..."
+if [ -d "$favicon_folder" ]; then
+    rm -r "$favicon_folder"
 fi
+mkdir -p "$favicon_folder"
+cp -r ./deploy/tools/favicon-generator/output/* "$favicon_folder"
+#fi
