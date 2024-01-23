cc709d706d60397778694a65f5e84207fb26d42e
wbh1328551759 <1328551759@qq.com>
2024-01-23 11:40:33 +0800

    feat: fix favicon_generator.sh

--- deploy/scripts/favicon_generator.sh
+++ deploy/scripts/favicon_generator.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #master_url="${FAVICON_MASTER_URL:-$NEXT_PUBLIC_NETWORK_ICON}"
-master_url='$NEXT_PUBLIC_NETWORK_ICON'
+master_url=$NEXT_PUBLIC_NETWORK_ICON
+echo 'master_url: ' $master_url
 export MASTER_URL="$master_url"
 
 dotenv \
